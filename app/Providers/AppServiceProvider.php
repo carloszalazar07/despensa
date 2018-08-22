c60f9f5e058bf977f49f05bc874e753071bcdb25
Carlos <zalazaarcarlos@gmail.com>
2018-08-22 19:18:39 -0300

    laravel sin package y con las migracion

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-    }
+        Schema::defaultStringLength(190);    }
 
     /**
      * Register any application services.
